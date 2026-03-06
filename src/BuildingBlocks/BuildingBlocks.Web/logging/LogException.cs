576b664e9cae99c1d5c1e659c4c33f5d5862d6e8
smrithwindev <rithwindev2002@gmail.com>
2026-03-06 14:24:01 +0530

    feat(buildingblocks-web): add centralized exception logging utility using Serilog

---
+++ src/BuildingBlocks/BuildingBlocks.Web/logging/LogException.cs
@@ -0,0 +1,23 @@
+﻿// we don't want to register to Dependency Injection Container
+// we make it public static so that it can be accessed globally
+using Serilog;
+
+namespace BuildingBlocks.Web.Logging
+{
+    public static class LogException
+    {
+        // Anytime an exception occurs anywhere in the application it would be passed to this method and assigned via its constructor
+        public static void LogExceptions(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToDebugger(ex.Message);
+            LogToConsole(ex.Message);
+        }
+
+        public static void LogToFile(string message) => Log.Information(message);
+
+        public static void LogToDebugger(string message) => Log.Debug(message);
+
+        public static void LogToConsole(string message) => Log.Warning(message);
+    }
+}
