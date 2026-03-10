bcd8a15c67bf1bc3a2df729d631c219e29bed763
smrithwindev <rithwindev2002@gmail.com>
2026-03-10 19:35:34 +0530

    feat(product-domain): introduce Product entity

---
+++ src/Services/Product/ProductApi.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApi.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
