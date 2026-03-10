b02b3d29ceb5a1480800f3acf0c6e9e6d7ba3f51
smrithwindev <rithwindev2002@gmail.com>
2026-03-10 20:12:10 +0530

    feat(product-application): define product repository contract

---
+++ src/Services/Product/ProductApi.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Core.Interfaces;
+using ProductApi.Domain.Entities;
+
+namespace ProductApi.Application.Interfaces
+{
+    public interface IProductRepository : IGenericInterface<Product> { }
+}
