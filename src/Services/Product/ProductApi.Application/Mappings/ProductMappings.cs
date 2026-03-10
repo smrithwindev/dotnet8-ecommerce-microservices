9281d843412f6512ad8a39cb594c3741f79cea12
smrithwindev <rithwindev2002@gmail.com>
2026-03-10 20:06:16 +0530

    feat(product-application): implement product entity–DTO mappings

---
+++ src/Services/Product/ProductApi.Application/Mappings/ProductMappings.cs
@@ -0,0 +1,46 @@
+﻿using ProductApi.Application.DTOs;
+using ProductApi.Domain.Entities;
+
+namespace ProductApi.Application.Mappings
+{
+    public class ProductMappings               //// here we are not using AutoMapper for simplicity, we are doing manual mapping
+    {
+        public static Product ToEntity(ProductDTO product) => new()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Quantity = product.Quantity,
+            Price = product.Price
+        };
+
+        public static (ProductDTO, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product> products)
+        {
+            if (product is not null || products is null)
+            {
+                var singleProduct = new ProductDTO
+                (
+                    Id: product.Id,
+                    Name: product.Name!,
+                    Quantity: product.Quantity,
+                    Price: product.Price
+                );
+                return (singleProduct, null);
+            }
+
+            if (products is not null || product is null)
+            {
+                var _products = products!.Select(p =>
+                new ProductDTO
+                (
+                    p.Id,
+                    p.Name!,
+                    p.Quantity,
+                    p.Price
+                )).ToList();
+                return (null!, _products);
+            }
+
+            return (null!, null);
+        }
+    }
+}
