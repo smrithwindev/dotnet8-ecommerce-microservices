b093a1f0fd1ee3a8ea7c2f088cf99d70140cc96a
smrithwindev <rithwindev2002@gmail.com>
2026-03-23 18:17:11 +0530

    feat(infrastructure): add ServiceContainer to register ProductDbContext and ProductRepository

---
+++ src/Services/Product/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.Web.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder; // Ensure this is included for IApplicationBuilder
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            // Register your infrastructure services here
+            //For example: services.AddScoped<IProductRepository, ProductRepository>();
+
+            SharedServiceContainer.AddSharedServices<ProductDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //Register middleware such as:
+            //Global Exception : Handles external errors.
+            //Listen to Only Api Gateway : blocks all outsider calls;
+
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
