02c9419abd815775c34386b62c5dfeb5db1b8b2a
smrithwindev <rithwindev2002@gmail.com>
2026-04-04 19:12:45 +0530

    feat(products): add get all products endpoint

---
+++ src/Services/Product/ProductApi.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using BuildingBlocks.Core.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Application.DTOs;
+using ProductApi.Application.Interfaces;
+using ProductApi.Application.Mappings;
+
+namespace ProductApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAllProducts()
+        {
+            //Get all products from the repo
+            var products = await _productRepository.GetAllAsync();
+            if (!products.Any())
+            {
+                return NotFound("No products found in the database");
+            }
+            //convert data from entity to DTOList
+            var response = ProductMappings.ToDtoList(products);
+            return Ok(response);
+        }
+
+
+    }
+}
